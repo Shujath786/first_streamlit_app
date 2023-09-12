19d84a0de6b131594bc2f00ecc1577bc3dceb272
Mohammed Shujauddin <Shujath786@users.noreply.github.com>
2023-09-12 01:18:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,4 +64,3 @@ try:
 #streamlit.write('Thanks for adding', add_my_fruit)
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
-
