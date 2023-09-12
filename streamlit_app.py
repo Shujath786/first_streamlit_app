94254a3ea511ae141dbe8a2a4a5878cd09f87e81
Mohammed Shujauddin <Shujath786@users.noreply.github.com>
2023-09-12 01:02:17 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
-
+```
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -38,7 +38,7 @@ try:
       streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
     streamlit.error()
-
+```
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
